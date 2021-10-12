d7baad5d73f2059e7d17b5e4cf22674dc087c113
João Pedro Souza <jsouza.1650@gmail.com>
2021-10-12 19:36:53 -0300

    created menu component

---
+++ src/components/Menu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="menu">
+    <img
+      class="logo"
+      src="../assets/icons/twitter_white.svg"
+      alt=""
+      srcset=""
+    />
+
+    <div class="buttons-container">
+      <button>
+        <img
+          class="button-icon"
+          src="../assets/icons/explore_icon.svg"
+          alt=""
+        />
+        <span>Explorar</span>
+      </button>
+      <button>
+        <img
+          class="button-icon"
+          src="../assets/icons/settings_icon.svg"
+          alt=""
+        />
+        <span>Configurações</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+};
+</script>
+
+<style scoped>
+.menu {
+  width: 30vw;
+  padding-left: 50px;
+  display: flex;
+  flex-direction: column;
+}
+
+.logo {
+  width: 28px;
+  height: 28px;
+  margin-top: 20px;
+	margin-left: 10px;
+}
+
+.buttons-container {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+button {
+	width: 200px;
+  height: 50px;
+  margin-bottom: 15px;
+	padding: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: white;
+  text-align: start;
+  border-radius: 30px;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: rgba(255, 255, 255, 0.096);
+}
+
+button span {
+  font-family: "Open Sans", sans-serif;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+.button-icon {
+  padding-right: 10px;
+}
+</style>
